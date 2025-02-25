eb238c28a5e7f7a06d3c824878b2171018eee97e
giovanaassis <giovana_2011_11@hotmail.com>
2025-02-25 00:14:13 -0300

    feat: add yup validation

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,12 +1,29 @@
 import { Request, Response } from "express";
+import * as yup from "yup";
 
 interface ICidade {
     nome: string
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+})
 
-    console.log(req.body.nome);
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+
+    let validatedData: ICidade | undefined = undefined;
+    try {
+        validatedData = await bodyValidation.validate(req.body);
+        console.log(req.body.nome);
+        res.send('Criado!');
+
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+
+        res.json({
+            errors: yupError.message
+        });
+    }
+
     
-    res.send('Criado!');
 }
