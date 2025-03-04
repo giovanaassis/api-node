7ff8ca926f02cf27f08962988d2a2bd63b434d42
giovanaassis <giovana_2011_11@hotmail.com>
2025-03-04 13:24:15 -0300

    fix: fixed an error on config jest

--- src/tests/jest.setup.ts
+++ src/tests/jest.setup.ts
@@ -1,4 +1,13 @@
 import supertest from "supertest";
 import app from "../server/server";
+import { Knex } from "../server/database/knex";
+
+beforeAll(async () => {
+    await Knex.migrate.latest();
+})
+
+afterAll(async () => {
+    await Knex.destroy();
+})
 
 export const testServer = supertest(app);
