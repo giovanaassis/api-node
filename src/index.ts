87364ea588dc47c8eaba7b82ad59feed29464d04
giovanaassis <giovana_2011_11@hotmail.com>
2025-03-05 23:25:25 -0300

    fix: update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
+import { Knex } from "./server/database/knex";
 import app from "./server/server";
-import 'dotenv/config';
+import "dotenv/config";
 
-const port = process.env.PORT || 3333;
+const startServer = () => {
+  const port = process.env.PORT || 3333;
+  app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+};
 
-app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+if (process.env.IS_LOCALHOST !== 'true') {
+   Knex.migrate.latest()
+    .then(() => startServer)
+    .catch(console.log) ;
+} else {
+    startServer();
+}
+
+
