9406d960fd91b28c105dc29dff13d342802f6a55
giovanaassis <giovana_2011_11@hotmail.com>
2025-03-06 19:47:17 -0300

    feat: add routes for pessoas

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,7 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import { Router } from "express";
 
-import { CidadesControllers } from "../controllers";
+import { CidadesControllers, PessoasControllers } from "../controllers";
 
 const router = Router();
 
@@ -11,12 +11,16 @@ router.get('/', (req, res) => {
 
 // rotas para cidades
 router.get('/cidades', CidadesControllers.getAllValidation,CidadesControllers.getAll)
-router.get('/cidades/:id', CidadesControllers.getByIdlValidation,CidadesControllers.getById)
+router.get('/cidades/:id', CidadesControllers.getByIdValidation,CidadesControllers.getById)
 router.post('/cidades', CidadesControllers.createValidation, CidadesControllers.create);
 router.put('/cidades/:id', CidadesControllers.updateByIdValidation, CidadesControllers.updateById);
 router.delete('/cidades/:id', CidadesControllers.deleteByIdValidation, CidadesControllers.deleteById);
 
 // rotas para pessoas
-
+router.get('/pessoas', PessoasControllers.getAllValidation,PessoasControllers.getAll)
+router.get('/pessoas/:id', PessoasControllers.getByIdValidation,PessoasControllers.getById)
+router.post('/pessoas', PessoasControllers.createValidation, PessoasControllers.create);
+router.put('/pessoas/:id', PessoasControllers.updateByIdValidation, PessoasControllers.updateById);
+router.delete('/pessoas/:id', PessoasControllers.deleteByIdValidation, PessoasControllers.deleteById);
 
 export { router };
